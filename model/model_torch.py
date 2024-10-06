7ca8acf4f0bd4cc3300868209ad9576a28ce7f79
Phoenizard <jiangxiaoyi2004@gmail.com>
2024-10-06 12:50:38 +0100

    add bias = False

--- model/model_torch.py
+++ model/model_torch.py
@@ -12,19 +12,18 @@ class DivideByConstantLayer(nn.Module):
 class Model(nn.Module):
     def __init__(self, m, D):
         super(Model, self).__init__()
-        self.linear1 = torch.nn.Linear(D + 1, m)
-        self.relu = torch.nn.ReLU()
-        self.linear2 = torch.nn.Linear(m, 1)
+        self.linear1 = torch.nn.Linear(D + 1, m, bias=False)
+        self.tanh = torch.nn.Tanh()
+        self.linear2 = torch.nn.Linear(m, 1, bias=False)
         self.divide = DivideByConstantLayer(m)
 
         # HE initialization
         torch.nn.init.kaiming_normal_(self.linear1.weight)
         torch.nn.init.kaiming_normal_(self.linear2.weight)
         
-        
     def forward(self, x):
         fc1 = self.linear1(x)
-        fc2 = self.relu(fc1)
+        fc2 = self.tanh(fc1)
         fc3 = self.linear2(fc2)
         fc4 = self.divide(fc3)
         return fc4
